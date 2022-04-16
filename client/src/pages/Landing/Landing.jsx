8ceed78e934a922dde9d3bad98da8ba42c2f6eb1
Marlon De La Roche <marlondelaroch3@gmail.com>
2022-04-16 13:13:49 -0500

    crear componentes a partir de imágenes SVG

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./landing.module.css";
+import Fondo from "../../components/Designs/Fondo/Fondo";
 
 export default function Landing() {
   return (
-    <>
-      <div className={styles.bg}></div>;
-    </>
+    <div className={styles.bg}>
+      <Fondo width="1360px" heigth="663px">
+      </Fondo>
+    </div>
   );
 }
