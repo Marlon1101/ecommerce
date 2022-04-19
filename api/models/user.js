ed71769ef2637ac557b03796264f327011dab951
Marlon De La Roche <marlondelaroch3@gmail.com>
2022-04-19 02:03:59 -0500

    crear base de datos, conectar con la API y crear modelo de ejemplo

---
+++ api/models/user.js
@@ -0,0 +1,17 @@
+const { model, Schema } = require("mongoose");
+  
+const userSchema = new Schema({
+    name: String,
+    lastName: String,
+    email: String,
+    password: String
+  })
+
+const User = model("User", userSchema)
+
+module.exports = {
+    User,
+}
+
+
+
